5ce45a6e31111265a19be86bfa2f9f07eee402a5
svyatokshin <svyatokshin@gmail.com>
2021-06-24 14:07:35 -0400

    fixed issue with changing players when a square thats occupied is clicked

--- game-app/src/App.js
+++ game-app/src/App.js
@@ -3,22 +3,23 @@ import './App.css';
 import {useEffect, useState} from 'react';
 import Square from './components/Square';
 import {Paterns, Patterns} from './Patterns';
+import Depaulo from './images/Depaulo.jpg';
 
 function App() {
-  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""])
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState("O");
-  const [result, setResult] = useState({winner: "No One", state: ""})
+  const [result, setResult] = useState({winner: "No One", state: ""});
 
   useEffect(() => {
     checkIfWin();
     checkTie();
-
     if (player === 'X'){
       setPlayer('O')
     }
     else {
       setPlayer('X')
-    }
+      }
+    
   }, [board]);
 
   useEffect(() => {
@@ -28,14 +29,23 @@ function App() {
   }, [result])
 
   const squareDecision = (square) => {
+    
     setBoard(
       board.map((val, index) => {
       if (index === square && val === "") {
         return player;
       }
+      if (index === square && val !== ""){
+        if (player === 'X'){
+          setPlayer('O')
+        }
+        else {
+          setPlayer('X')
+        }
+      }
       return val;
-    })
-    );
+      })
+    )
   }
 
   const checkIfWin = () => {
@@ -82,19 +92,19 @@ function App() {
       <h2> Winner: {result.winner} {result.state}</h2>
       <div className="board">
         <div className="row">
-          <Square val={board[0]} squareDecision={() => {squareDecision(0)}}/>
-          <Square val={board[1]} squareDecision={() => {squareDecision(1)}}/>
-          <Square val={board[2]} squareDecision={() => {squareDecision(2)}}/>
+          <Square val={board[0]} squareDecision={() => squareDecision(0)}/>
+          <Square val={board[1]} squareDecision={() => squareDecision(1)}/>
+          <Square val={board[2]} squareDecision={() => squareDecision(2)}/>
         </div>
         <div className="row">
-          <Square val={board[3]} squareDecision={() => {squareDecision(3)}}/>
-          <Square val={board[4]} squareDecision={() => {squareDecision(4)}}/>
-          <Square val={board[5]} squareDecision={() => {squareDecision(5)}}/>
+          <Square val={board[3]} squareDecision={() => squareDecision(3)}/>
+          <Square val={board[4]} squareDecision={() => squareDecision(4)}/>
+          <Square val={board[5]} squareDecision={() => squareDecision(5)}/>
         </div>
         <div className="row">
-          <Square val={board[6]} squareDecision={() => {squareDecision(6)}}/>
-          <Square val={board[7]} squareDecision={() => {squareDecision(7)}}/>
-          <Square val={board[8]} squareDecision={() => {squareDecision(8)}}/>
+          <Square val={board[6]} squareDecision={() => squareDecision(6)}/>
+          <Square val={board[7]} squareDecision={() => squareDecision(7)}/>
+          <Square val={board[8]} squareDecision={() => squareDecision(8)}/>
         </div>
       </div>
     </div>
