244f4de395058cac01d2efbae71aab19ef692d10
svyatokshin <svyatokshin@gmail.com>
2021-07-15 17:03:01 -0400

    added ryans face as the O

--- game-app/src/components/Square.js
+++ game-app/src/components/Square.js
@@ -1,10 +1,13 @@
 import React, {useEffect} from 'react';
+import Depaulo from '../images/Depaulo.jpg'
 import '../App.css'
 
 function Square({ val, squareDecision }) {
+    let value = val;
+    console.log('val', value);
     return(
         <div className="square" onClick={squareDecision}>
-            {val}
+            { val == "O" ? <img src={Depaulo}/>: <h5>{val}</h5>}
         </div>
     )
 }
